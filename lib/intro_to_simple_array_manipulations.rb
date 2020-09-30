9195902d5609dc66f8136a7ac7e3a98a3d5715fb
rational-architecture-1641 <jarmah241@gmail.com>
2020-09-30 18:26:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,11 @@ end
 def using_flatten(arrray)
   array.flatten
 end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, int)
+  array.delete_at(int)
+end
